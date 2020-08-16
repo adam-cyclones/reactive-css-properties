e2cb21ae01b9530e017a296ba2da36fc35f42b11
Adam Crockett <adam.crockett@forgerock.com>
2020-08-16 20:36:14 +0100

    rougthly prepared for first release

---
+++ README.md
@@ -0,0 +1,75 @@
+# Reactive CSS Properties
+
+## The case for reactive-css-properties
+CSS in JavaScript is not essential with the advent of CSS custom properties (CSS variables), This is because a developer can `set` and `get` css custom properties from JavaScript or update existing variables defined in stylesheets.
+
+JavaScript and CSS can now share easily data bi-directionally, the result is that styles can remain in stylesheets and JavaScript can continue to add the gloss and sparkles.
+
+## Why reactive?
+If you open devtools, change a css variable, this change happens in realtime, JavaScript is unable to detect this change. So we need a workaround, Using reactive-css-properties, an observable gets set to watch for `style` attribute changes of the specified root element, JavaScript can then respond in realtime, diffing the `oldValue` vs `value` then calling a function if changes have been detected.
+
+## What is the use case?
+- Change one variable and have JavaScript set an entire theme's worth of variables.
+- Update a css variable based on screen position or any sensor / event that JavaScript can access that CSS cannot
+- Get CSS variables values and use them in logic
+- Separation of concerns between styling and business logic
+- Dry code
+- CSS can be used as configuration which is isomorphic
+- More efficient and natural feeling workflow 
+- Encourage developers to think differently and style variables instead of individual elements on the page, it might just change how you create apps and websites
+
+Related reading:
+- [Javascript Enhanced scss mixins concepts explained](https://dev.to/adam_cyclones/javascript-enhanced-scss-mixins-concepts-explained-3mpo)
+- [Reactive CSS Explained](https://dev.to/adam_cyclones/great-scott-reactive-css-231m)
+
+
+### Usage
+Set a css custom property.
+``` js
+import rCSSProps from "reactive-css-properties";
+
+// rCSSProps object has no undefined properties and will always yield a class-like callable object.
+// The name of your destructured variable is the name of the css variable in snake-case
+const { themeTextColor } = rCSSProps();
+
+// insert `--theme-text-color: #000;` into the root element `style` (<html> in this example).
+themeTextColor("#000");
+
+// or provide a fallback for browsers which do not support css custom properties
+themeTextColor("#000", "#000");
+```
+
+Watch for changes
+``` js
+// You must subscribe before changing values
+themeTextColor.subscribe((change) => {
+  console.log("Im watching for theme text changes", change);
+});
+```
+
+---
+
+### How to use a reactive variable (optional)
+#### From stylesheets
+Use the variable within `css`, `sass` or anything which results in css
+``` css
+body {
+    color: var(--theme-text-color);
+}
+```
+You could even define the variable before JavaScript has loaded, this is recommended to provide some styling, also to help keep track of variables in use.
+``` css
+:root {
+    --theme-text-color: #C0FF33;
+}
+```
+#### From JavaScript
+CSS in JavaScript is not essential, however it is useful, with the workflow described above, you provide styling with or without JavaScript, removing one of the major cases against CSS in JS.
+Using the variable is in a js string is simple, just cast it to a string.
+``` js
+const styles = `
+    body {
+        color: ${themeTextColor};
+    }
+`
+```
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,81 @@
-// please note that code sandbox relead doesnt play well,
-// plesae reload the page after each change if playing around -->.
+import rxDom from "rx-dom";
+import { camelToSnakeCase } from "./utils/camelToSnakeCase";
+import { callable } from "./utils/Callable";
+import { StringCastable, IHasValueOf } from "./types/utils";
+import {
+  ICSSPropCallbackChangeDetail,
+  CSSPropEntity,
+  ICSSPropCallable
+} from "./types/reactiveCSS";
 
-import rCSSProps from "./reactiveCSSProps";
+const fromMutationObserver = rxDom.DOM.fromMutationObserver;
 
-const { themeTextColor } = rCSSProps();
+/**
+ * Set css variables and react to changes
+ * */
+export default (
+  rootEl: HTMLElement = document.documentElement
+): { [k: string]: CSSPropEntity } => {
+  /**
+   * Tracks previous values against current values
+   * */
+  const previousValues: { [key: string]: ICSSPropCallbackChangeDetail } = {};
+  // observe rootEl for style changes
+  const rootStyleOvserver = fromMutationObserver(rootEl, {
+    attributes: true,
+    attributeFilter: ["style"],
+    attributeOldValue: false,
+    childList: false,
+    subtree: false
+  });
 
-// watch for changes
-themeTextColor.subscribe((change) => {
-  console.log("Im watching for theme text changes", change);
-});
-
-// set the variable and inject into the dom
-themeTextColor("#000");
-
-setTimeout(() => {
-  // change the theme text color after 3 seconds to simulate dark mode toggled
-  themeTextColor("#fff", "yellow");
-}, 3000);
-
-// just stringify it, its that easy!
-console.log(`
-  background: ${themeTextColor};
-`);
-
-console.log("JavaScript bend to thy will.");
+  /**
+   * used to return a magic object, an object where every got key returns a css variable factory function
+   */
+  const magicGetterFactory = Object.create(null);
+  return new Proxy(magicGetterFactory, {
+    get(target: { [key: string]: CSSPropEntity }, prop: string, receiver) {
+      const key = `--${camelToSnakeCase(prop)}`;
+      // always return a Callable
+      // @ts-ignore
+      target[prop] = callable<
+        ICSSPropCallable & IHasValueOf<string>,
+        Array<StringCastable>,
+        void
+      >({
+        function(value: StringCastable, fallbackValue?: StringCastable): void {
+          previousValues[key] = {
+            value,
+            oldValue: previousValues[key] ? previousValues[key].value : null
+          };
+          // set CSS variable value to DOM
+          if (fallbackValue) {
+            rootEl.style.setProperty(key, `${value}, ${fallbackValue}`);
+          } else {
+            rootEl.style.setProperty(key, value.toString());
+          }
+        },
+        subscribe(cb: (change: ICSSPropCallbackChangeDetail) => void) {
+          rootStyleOvserver.subscribe((change) => {
+            // @ts-ignore
+            const mutation = change[0];
+            const { oldValue } = previousValues[key];
+            const newValue = window
+              .getComputedStyle(mutation.target)
+              .getPropertyValue(key);
+            if (oldValue !== newValue) {
+              cb({
+                value: newValue,
+                oldValue: oldValue
+              });
+            }
+          });
+        },
+        valueOf() {
+          return `var(${key})`;
+        }
+      });
+      return Reflect.get(target, prop, receiver);
+    }
+  });
+};
